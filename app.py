389febdf12231739cfe8eff2e224e33932e3762e
Unpack Python <100990278+unpackpy@users.noreply.github.com>
2022-03-05 07:28:25 +0000

    Created a small project

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.title("Hello, you!")
+
+st.write("How are you today my friend?")
+
+st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Python_logo_and_wordmark.svg/486px-Python_logo_and_wordmark.svg.png")
+
+# st.button("Let it snow, let it snow, let it snow", on_click=st.snow)
