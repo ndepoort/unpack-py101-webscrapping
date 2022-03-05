0886fc9742e5781382b7d49fdd50f74ef4057cb0
Unpack Python <100990278+unpackpy@users.noreply.github.com>
2022-03-05 07:47:01 +0000

    Added button to make it snow🌨

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ st.write("How are you today my friend?")
 
 st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Python_logo_and_wordmark.svg/486px-Python_logo_and_wordmark.svg.png")
 
-# st.button("Let it snow, let it snow, let it snow", on_click=st.snow)
+st.button("Let it snow, let it snow, let it snow", on_click=st.snow)
